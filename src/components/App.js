030d559e00d8122e9c5a752e0d196f4a6979dbce
Ilayda <ilayda.bakare@gmail.com>
2020-12-01 18:41:39 +0000

    add 404 page but didn't route it, plus meta tags

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,19 @@ import Banner from './Banner';
 import Section from './Section';
 import Footer from './Footer';
 import './stylesheets/_index.scss';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Banner/>
-      <Section/>
-      <Footer/>
-    </React.Fragment>
+    <BrowserRouter>
+      <React.Fragment>
+        <Banner/>
+        <Section/>
+        <Footer/>
+      </React.Fragment>
+    </BrowserRouter>
   );
 };
 
