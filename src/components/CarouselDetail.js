e09bb85f52fc707d8bc65a88d825135c9d6d3d3e
Ilayda <ilayda.bakare@gmail.com>
2020-12-21 11:55:19 +0000

    added 15 days of lockdown project

--- src/components/CarouselDetail.js
+++ src/components/CarouselDetail.js
@@ -4,6 +4,10 @@ import exhibitz3 from '../images/exhibitz3.png';
 import boatz1 from '../images/boatz1.png';
 import boatz2 from '../images/boatz2.png';
 import boatz3 from '../images/boatz3.png';
+import lockdown1 from '../images/lockdown1.png';
+import lockdown2 from '../images/lockdown2.png';
+import lockdown3 from '../images/lockdown3.png';
+
 
 const websiteConfig = {
   exhibitz: {
@@ -13,44 +17,68 @@ const websiteConfig = {
       image: exhibitz1,
       header: "Browse exhibits",
       caption: "first slide",
-      alt: "this is the first screenshot"
+      alt: "Exhibitz homepage"
     },
     slide2: {
       image: exhibitz2,
       header: "Browse exhibits",
       caption: "second slide",
-      alt: "this is the second screenshot"
+      alt: "Details of an exhibition"
     },
     slide3: {
       image: exhibitz3,
       header: "Browse exhibits",
       caption: "third slide",
-      alt: "this is the third screenshot"
+      alt: "Map showing museums"
     }
   },
 
   boatz: {
     websiteName: "Boatz",
-    text: "temporary museum exhibits (pre-corona) in one place!",
+    text: "A place to rent boats!",
     slide1: {
       image: boatz1,
-      header: "Browse exhibits",
-      caption: "first slide",
+      header: "",
+      caption: "",
       alt: "this is the first screenshot"
     },
     slide2: {
       image: boatz2,
-      header: "Browse exhibits",
-      caption: "second slide",
+      header: "",
+      caption: "",
       alt: "this is the second screenshot"
     },
     slide3: {
       image: boatz3,
-      header: "Browse exhibits",
-      caption: "third slide",
+      header: "",
+      caption: "",
       alt: "this is the third screenshot"
     }
+  },
+
+  lockdown: {
+    websiteName: "Fifteen Days of Lockdown",
+    text: "A digital exhibition by Charlotte Holme",
+    slide1: {
+      image: lockdown1,
+      header: "",
+      caption: "",
+      alt: "homepage"
+    },
+    slide2: {
+      image: lockdown2,
+      header: "",
+      caption: "",
+      alt: "about the exhibition"
+    },
+    slide3: {
+      image: lockdown3,
+      header: "",
+      caption: "",
+      alt: "photos made by artist"
+    }
   }
+
 };
 
 
